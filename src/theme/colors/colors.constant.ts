1f919175c884ed91ed65ed579546e789f98803d6
sarkisovTu <sarkisov1337@gmail.com>
2022-09-04 18:00:10 +0300

    feat: app colors

---
+++ src/theme/colors/colors.constant.ts
@@ -0,0 +1,10 @@
+export const COLOR_BLACK = '#000000';
+
+export const COLOR_WHITE = '#FFFFFF';
+
+export const COLOR_RED = '#DD3333';
+
+export const COLOR_GREY_100 = '#E8E8E8';
+export const COLOR_GREY_200 = '#D9D9D9';
+export const COLOR_GREY_300 = '#787878';
+export const COLOR_GREY_400 = '#595959';
