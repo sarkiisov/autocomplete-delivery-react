dd1f4b9524c47b4448ed3d5eb1d40aa9306845d9
sarkisovTu <sarkisov1337@gmail.com>
2022-09-05 18:26:16 +0300

    feat: svg icons & base component

---
+++ src/theme/icons/components/BaseSVGIcon.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+
+export interface BaseSVGIconProps extends React.SVGProps<SVGSVGElement> {
+  viewBox?: string;
+  width?: string;
+  height?: string;
+  fill?: string;
+  stroke?: string;
+  htmlColor?: string;
+}
+
+export const BaseSVGIcon: React.FC<BaseSVGIconProps> = ({
+  children,
+  viewBox = '0 0 24 24',
+  width,
+  height,
+  fill,
+  stroke,
+  htmlColor
+}) => {
+  return (
+    <svg
+      viewBox={viewBox}
+      width={width}
+      height={height}
+      fill={fill}
+      stroke={stroke}
+      color={htmlColor}
+    >
+      {children}
+    </svg>
+  );
+};
---
+++ src/theme/icons/components/XLogo.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { BaseSVGIcon } from './BaseSVGIcon';
+import { IconProps } from '../icons.types';
+
+import { COLOR_GREY_300 } from '../../colors';
+
+export const XLogo: React.FC<IconProps> = (props) => (
+  <BaseSVGIcon
+    width="24px"
+    height="24px"
+    fill="none"
+    stroke= {COLOR_GREY_300}
+    {...props}
+  >
+    <path xmlns="http://www.w3.org/2000/svg" d="M21.3333 2.66667L2.66663 21.3333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    <path xmlns="http://www.w3.org/2000/svg" d="M2.66663 2.66667L21.3333 21.3333" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  </BaseSVGIcon>
+);
