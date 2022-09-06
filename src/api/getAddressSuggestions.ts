9e271bdae56388b0bda74cd48ef8ef681ac0c5f7
sarkisovTu <sarkisov1337@gmail.com>
2022-09-06 20:44:13 +0300

    feat: fetch Dadata address API

---
+++ src/api/getAddressSuggestions.ts
@@ -0,0 +1,25 @@
+type BoundNames = 'country' | 'region' | 'area' | 'city' | 'settlement' | 'street' | 'house' | 'flat';
+type FromBound = BoundNames;
+type ToBound = Exclude<BoundNames, 'flat'>;
+
+export async function getAddressSuggestions(query: string, fromBound: FromBound, toBound: ToBound) {
+  return fetch('https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Token ${process.env.REACT_APP_DADATA_API_KEY}`
+    },
+    body: JSON.stringify({
+      'query': query,
+      'from_bound': { value: fromBound },
+      'to_bound': { value: toBound }
+    })
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+
+    throw new Error('An error occurred executing the API call');
+  });
+}
