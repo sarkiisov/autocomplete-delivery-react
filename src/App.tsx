9611edcc066c6222da969244e0f41ae9f7d6264b
sarkisovTu <sarkisov1337@gmail.com>
2023-01-28 18:08:52 +0300

    feat: add ToastContainer to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { Provider as ReduxProvider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ToastContainer } from 'react-toastify';
 import { store } from './store/configureStore';
 
 import useTheme from './theme/global';
 import useStyles from './App.styles';
 
+import { toastContainerProps } from './components/Toastify/Toastify.helpers';
 import { Form } from './containers/Form';
 
 const queryClient = new QueryClient();
@@ -21,6 +23,7 @@ function App() {
         <div className="App">
           <Form className={classes.form} />
         </div>
+        <ToastContainer {...toastContainerProps} />
       </ReduxProvider>
     </QueryClientProvider>
   );
