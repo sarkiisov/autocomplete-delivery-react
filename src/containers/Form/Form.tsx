fca2b5ee113ffe11c1209f8544b89fa0d6b7638d
sarkisovTu <sarkisov1337@gmail.com>
2023-02-08 15:27:00 +0300

    refactor: Form container

--- src/containers/Form/Form.tsx
+++ src/containers/Form/Form.tsx
@@ -1,34 +1,36 @@
 import React from 'react';
+import clsx from 'clsx';
+import hash from 'object-hash';
 
 import { Formik, Form as FormikForm, Field } from 'formik';
-import { fieldConfig, initialValues, validationSchema } from './config/fieldsConfig';
-import { FiledProps } from '../../types/form';
+import { fieldConfig, initialValues, validationSchema } from '../../config/form';
 
 import { Button } from '../../components/Button';
-import { setNotification } from '../../utils/setNotification';
+import { setNotification } from '../../utils';
+import { addBookItem } from '../../store/addressBook/reducer';
+import { useAppDispatch } from '../../store/hooks';
+
+import useStyles from './Form.styles';
 
 export interface FormProps {
   className?: string;
 }
 
 export const Form: React.FC<FormProps> = ({ className }) => {
-  const handleSubmit = (values) => {
-    setNotification({ type: 'success', message: 'Заказ успешно создан' });
-    console.log(JSON.stringify(values, null, 2));
+  const classes = useStyles();
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = (details, { resetForm }) => {
+    const id = hash(details);
+    const timestamp = Date.now();
+
+    dispatch(addBookItem({ id, timestamp, details }));
+    setNotification({ type: 'success', message: 'Адрес успешно добавлен' });
+    resetForm();
   };
 
-  const renderField = (field: FiledProps, errors, touched) => (
-    <Field
-      as={field.as}
-      name={field.name}
-      key={field.name}
-      error={touched[field.name] && Boolean(errors[field.name])}
-      {...field.props}
-    />
-  );
-
   return (
-    <div className={className}>
+    <div className={clsx(className, classes.root)}>
       <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
@@ -37,11 +39,20 @@ export const Form: React.FC<FormProps> = ({ className }) => {
         validateOnChange
       >
         {({
-          isValid, dirty, touched, errors
+          values, isValid, dirty, touched, errors
         }) => (
           <FormikForm>
-            {fieldConfig.map((field) => renderField(field, errors, touched))}
-            <Button innerText="Оформить заказ" isDisabled={!(isValid && dirty)} />
+            {fieldConfig.map((field) => (
+              <Field
+                as={field.as}
+                value={values[field.name]}
+                name={field.name}
+                key={field.name}
+                error={touched[field.name] && Boolean(errors[field.name])}
+                {...field.props}
+              />
+            ))}
+            <Button isDisabled={!(isValid && dirty)}>Добавить адрес</Button>
           </FormikForm>
         )}
       </Formik>
