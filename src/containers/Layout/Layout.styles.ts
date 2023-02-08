9fb232f333be10f6be516c777a7d3ac2f8405fb8
sarkisovTu <sarkisov1337@gmail.com>
2023-02-08 15:27:53 +0300

    feat: Layout container

---
+++ src/containers/Layout/Layout.styles.ts
@@ -0,0 +1,14 @@
+import { createUseStyles } from 'react-jss';
+
+export interface LayoutStyleProps {
+
+}
+
+export default createUseStyles({
+  root: {
+    minWidth: '350px',
+    maxWidth: '430px',
+    padding: '16px',
+    margin: '0 auto'
+  }
+});
